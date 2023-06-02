d30fa41c4e7aa42a7c83e1176c7d67f1b65d975b
Амалия Тайлер <AATayler@1cbit.ru>
2023-06-02 18:08:15 +0300

    working on slider

--- js/script.js
+++ js/script.js
@@ -1,14 +1,65 @@
-let slides, prev, next, slidesWrapper;
+const slides = document.querySelectorAll('.slider-years__slide'),
+      prev = document.querySelector('.slider-arrow_prev'),
+      next = document.querySelector('.slider-arrow_next'),
+      slidesWrapper = document.querySelector('.slider-years__wrapper'), 
+      slidesField = document.querySelector('.slider-years__inner'),
+      width = window.getComputedStyle(slidesWrapper).width;
+      
 
-if(document.querySelector('.slider-years__body')) {
-    new Swiper('.slider-years__body', {
-        observer: true,
-        observerParents: true,
-        slidesPerView: 'auto',
-        navigation: {
-            nextEl: '.years__arrows .slider-arrow_next',
-            prevEl: '.years__arrows .slider-arrow_prev',
-        },
-        loop: true
-    });
+slidesField.style.width = 100 * slides.length + '%';
+slidesField.style.width = 580 + (60 * (slides.length - 1)) + ((slides.length - 1) * 100) + 'px';
+/*slidesField.width = 580px + (60px * (slides.length - 1)) + margin/padding */
+
+let offset = 0;
+
+function deleteNotDigits(str) {
+    return +str.replace(/\D/g, ''); /* regExp*/
 }
+
+slides.forEach(slide => {
+    slide.style.width = width;
+})
+
+next.addEventListener('click', () => {
+
+    console.log(offset)
+
+    if(offset == deleteNotDigits(width) * (slides.length - 1)) {
+        offset = 0;
+    } else {
+        offset += deleteNotDigits(width);
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+});
+
+prev.addEventListener('click', () => {
+    
+    console.log(offset)
+
+    if(offset == 0) {
+        offset = deleteNotDigits(width) * (slides.length - 1);
+    } else {
+        offset -= deleteNotDigits(width);
+    }
+
+    slidesField.style.transform = `translateX(-${offset}px)`;
+});
+
+
+
+
+// if(document.querySelector('.slider-years__body')) {
+//     new Swiper('.slider-years__body', {
+//         observer: true,
+//         observerParents: true,
+//         slidesPerView: 'auto',
+//         spaceBetween: 100,
+//         navigation: {
+//             enabled: true,
+//             nextEl: '.slider-arrow_next',
+//             prevEl: '.slider-arrow_prev',
+//         },
+//         loop: true
+//     });
+// }
